2441a85b201eccd545d8accadb1274d82efd1b82
AuroreC <avhelia@gmail.com>
2022-02-12 23:16:29 +0100

    finished part 4

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,30 +1,57 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import * as test from './layout.module.css'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import {
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText,
+    siteTitle,
+  } from './layout.module.css'
 
-const Layout = ({ pageTitle, children}) => {
+
+// ... import statements
+
+const Layout = ({ pageTitle, children }) => {
+    const data = useStaticQuery(graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `)
+  
     return (
-        <div className={test.container}>
-            <title>{pageTitle}</title>
-            <nav>
-                <ul className={test.navLinks}>
-                    <li className={test.navLinkItem}>
-                        <Link to="/" className={test.navLinkText}>
-                           Home
-                        </Link>
-                    </li>
-                    <li className={test.navLinkItem}>
-                        <Link to="/about" className={test.navLinkText}>
-                         About me</Link>
-                    </li>
-                </ul>
-            </nav>
-            <main>
-                <h1 className={test.heading}>{pageTitle}</h1>
-                {children}
-            </main>
-        </div>
+      <div className={container}>
+        <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+        <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+        <nav>
+          <ul className={navLinks}>
+            <li className={navLinkItem}>
+              <Link to="/" className={navLinkText}>
+                Home
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/about" className={navLinkText}>
+                About
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/blog" className={navLinkText}>
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <h1 className={heading}>{pageTitle}</h1>
+          {children}
+        </main>
+      </div>
     )
-}
-
-export default Layout
+  }
+  
+  export default Layout
